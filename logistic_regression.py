e3af2b8691a9562c2dfee2aa421136836619d52f
ugp <ugp@ugps-MacBook-3.local>
2015-07-15 17:34:12 -0500

    updated logistic model

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,69 +1,68 @@
 import pandas as pd  
-# import numpy as np
 import statsmodels.api as sm
-# import matplotlib.pyplot as plt 
 
-
+# Load in the Loans: 
 loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
 
-# Export to CSV: 
-#loansData.to_csv('loansData_clean.csv', header=True, index=False)
-
-
 # Clean Interest Rate: 
-cleanInterestRate = loansData['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 4))
-loansData['Interest.Rate'] = cleanInterestRate
+loansData['Interest.Rate'] = loansData['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 4))
 
-
- 
 # Clean Loan Length:
-cleanLoanLength = loansData['Loan.Length'].map(lambda x: int(x.rstrip(' months')))
-loansData['Loan.Length'] = cleanLoanLength
+loansData['Loan.Length'] = loansData['Loan.Length'].map(lambda x: int(x.rstrip(' months')))
 
 # Clean FICO Range
 cleanFicoRange = loansData['FICO.Range'].map(lambda x: x.split('-'))
-cleanFicoRange = cleanFicoRange.map(lambda x: [int(num) for num in x])
+cleanFicoRange = cleanFicoRange.map(lambda x: [int(num) for num in x]) #Review List Comp
 loansData['FICO.Range'] = cleanFicoRange
 
 # Create a New Column called FICO.Score: 
-fico_score = loansData['FICO.Range'].map(lambda x: x.pop(0)) #How calc midpoint? 
-loansData['FICO.Score'] = fico_score
+loansData['FICO.Score'] = loansData['FICO.Range'].map(lambda x: x.pop(0)) #How calc midpoint? 
 
-
-twelve_column = loansData['Interest.Rate'].map(lambda x: 1 if x<0.1200 else 0)
-# print twelve_column
-# print len(twelve_column)
-
-loansData['IR_TF'] = twelve_column
-print loansData['IR_TF'].head()
-
-#print loansData.head(1)
-
-# IT WORKED! http://stackoverflow.com/questions/14029245/python-putting-an-if-elif-else-statement-on-one-line
-# http://stackoverflow.com/questions/1585322/is-there-a-way-to-perform-if-in-pythons-lambda
-# 81174    0.0890   81174    1
-# 99592    0.1212	99592    0
-# 80059    0.2198	80059    0
-# 15825    0.0999	15825    1
-# 33182    0.1171	33182    1
-# Name: Interest.Rate, dtype: float64
-
+# Create Column to group loans Above and Below 12% interest rate:
+loansData['IR_TF'] = loansData['Interest.Rate'].map(lambda x: 1 if x<0.1200 else 0)
+#print loansData.head(10)
 
 
 # Statsmodels needs an intercept column in your dataframe, so add a column with a constant intercept of 1.0.
-constant_intercept = loansData['Interest.Rate'].map(lambda x: 1.0 if x >= 1  else 1.0)
-loansData['Constant.Intercept'] = constant_intercept
+# constant_intercept = loansData['Interest.Rate'].map(lambda x: 1.0 if x >= 1  else 1.0)
+# loansData['Constant.Intercept'] = constant_intercept
 
+loansData['Constant.Intercept'] = 1  
+#print loansData['Constant.Intercept'].head(10) - Entire column set to 1
 
-
-print loansData.head(10)
-
-
-
-
-# Does it literally mean a list? 
 # Create a list of the column names of our independent variables, including the intercept, and call it ind_vars
-ind_vars = [loansData['FICO.Score'], loansData['Amount.Funded.By.Investors']]
+ind_vars = [loansData['FICO.Score'], loansData['Amount.Funded.By.Investors'], loansData['Constant.Intercept']]
+
+# print loansData[0:0] Are all the Headers: 
+# Empty DataFrame
+# Columns: 
+	# [Amount.Requested, Amount.Funded.By.Investors, Interest.Rate, 
+	# Loan.Length, Loan.Purpose, Debt.To.Income.Ratio, State, Home.Ownership, 
+	# Monthly.Income, FICO.Range, Open.CREDIT.Lines, Revolving.CREDIT.Balance, 
+	# Inquiries.in.the.Last.6.Months, Employment.Length, FICO.Score, IR_TF, Constant.Intercept]
+# Index: []
+
+column_names = loansData[0:0]
+data_rows = loansData[1::]
+df = pd.DataFrame(data_rows, columns=column_names)
+
+
+
+
+
+# # Define the logistic Regression model: 
+logit = sm.Logit(df['IR_TF'], df[ind_vars])
+#logit = sm.Logit(loansData['IR_TF'], )
+
+# # Fit the model: 
+result = logit.fit()
+
+# # 3 - Get the fitted coefficients from the result: 
+coeff = result.params
+print coeff # Gives the coefficient of each independent (predictor) variable
+
+
+
 
 
 
